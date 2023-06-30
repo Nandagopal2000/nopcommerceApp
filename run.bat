6d3ebf416ce2693b344116db8c531ac37bcf3043
Nandagopal2000 <nandugopaldevji@gmail.com>
2023-06-30 10:45:38 +0530

    Updated

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-pytest -v -m "sanity" --html=Reports\report_chrome.html testCases --browser chrome
-pytest -v -m "regression" --html=Reports\report.html testCases --browser chrome 
+rem pytest -v -m "sanity" --html=Reports\report_chrome.html testCases --browser chrome
+rem pytest -v -m "regression" --html=Reports\report.html testCases --browser chrome 
 rem pytest -v -m "sanity and regression" --html=Reports\report.html testCases --browser chrome 
 rem pytest -v -m "sanity or regression" --html=Reports\report.html testCases --browser chrome 
 
@@ -7,3 +7,5 @@ rem pytest -v -m "sanity" --html=Reports\report_firefox.html testCases --browser
 rem pytest -v -m "regression" --html=Reports\report.html testCases --browser firefox
 rem pytest -v -m "sanity and regression" --html=Reports\report.html testCases --browser firefox 
 rem pytest -v -m "sanity or regression" --html=Reports\report.html testCases --browser firefox
+
+pytest -v -m --html=Reports\report_chrome.html testCases --browser chrome
