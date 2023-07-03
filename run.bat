502ed7cc86876ac921e30ef97f20df6a2c5ba3f7
Nandagopal D <62707156+Nandagopal2000@users.noreply.github.com>
2023-07-03 11:08:13 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -8,4 +8,4 @@ rem pytest -v -m "regression" --html=Reports\report.html testCases --browser fir
 rem pytest -v -m "sanity and regression" --html=Reports\report.html testCases --browser firefox 
 rem pytest -v -m "sanity or regression" --html=Reports\report.html testCases --browser firefox
 
-pytest -v -m "Data" --html=Reports\report.html testcases --browser chrome
+pytest -v -m "Data" --html=Reports\report.html testCases --browser chrome
