5d2d542fa5492e542a8890b789c5461be00af4fe
Nandagopal2000 <nandugopaldevji@gmail.com>
2023-07-05 16:24:44 +0530

    Updated

--- run.bat
+++ run.bat
@@ -10,6 +10,12 @@ rem pytest -v -m "sanity or regression" --html=Reports\report.html testCases --b
 
 
 rem pytest -v -m --html=Reports\report_chrome.html testCases --browser chrome
-pytest -v -m "Data1" --html=Reports\report_chrome.html testCases --browser chrome
-pytest -v -m "Data2" --html=Reports\report_chrome.html testCases --browser chrome
+rem pytest -v -m "Data1" --html=Reports\report_chrome.html testCases --browser chrome
+rem pytest -v -m "Data2" --html=Reports\report_chrome.html testCases --browser chrome
+
+pytest -v --html=Reports\newreport.html testCases/test_editCustomer.py --browser chrome
+pytest -v --html=Reports\newreport.html testCases/test_deleteCustomer.py --browser chrome
+
+
+
 
